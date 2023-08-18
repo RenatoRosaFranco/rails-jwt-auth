a324a7d545c0c5f603dfa2149297e273804612d0
Renato Rosa Franco <renato_ny@live.com>
2023-08-18 16:03:33 -0300

    Updating users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,9 +20,9 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     
     if user.save
-      render json: @user, status: :created
+      render json: user, status: :created
     else
-      render json: { errors: @user.errors.full_messages },
+      render json: { errors: user.errors.full_messages },
       status: :unprocessable_entity
     end
   end
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.permit(:user)
+    params.require(:user)
       .permit(:name, :username, :email, :password, :password_confirmation)
   end
 end
